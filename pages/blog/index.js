2d37569a9f58d8a321904c4f01671ccccc9f8f6f
Haniche Zahir <zahir.haniche@inetum.com>
2024-09-05 01:40:02 +0200

    Integration design blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
 
 export default function index() {
   return (
-    <div>index</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 my-5 text-center ">
+            <h1>Bienvenue sur le Blog</h1>
+            <p>Voici les articles</p>
+          </div>
+          <div className={` ${styles.tableau} col-4`}>
+            <h5>Lisez les articles</h5>
+            <p className="my-2">Chaque auteur tente de vous apporter...</p>
+            <Link href="/blog">
+                <span>Lire cet article</span>
+            </Link>
+          </div>
+        </div>      
+      </div>
   )
 }
