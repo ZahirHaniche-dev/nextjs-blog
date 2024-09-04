1eb92353a27d370fbc731b899db00dac5d6f04c6
Haniche Zahir <zahir.haniche@inetum.com>
2024-09-05 01:45:42 +0200

    Integration design Liste

--- pages/liste/index.js
+++ pages/liste/index.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
 
 export default function index() {
   return (
-    <div>index</div>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 my-5 text-center">
+          <h1>La Liste des Utilisateurs</h1>
+        </div>
+        {/* Utilisation de mx-auto pour centrer le bloc et d-flex pour espacer les éléments */}
+        <div className={` ${styles.tableau} col-6 mx-auto d-flex justify-content-between`}>
+          <span>Zahir</span>
+          <span>
+            <Link href="/contact">
+              <span>Contacter</span>
+            </Link>
+          </span>
+        </div>
+      </div>      
+    </div>
   )
 }
